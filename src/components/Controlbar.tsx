8f22ccf7362f980a0c8292746084ebc1f7a357ff
victor <victoratnz@outlook.com>
2024-08-31 11:39:34 +1200

    Add Controlbar component
    A new Controlbar component has been added to the project. This component will be responsible for rendering the control bar of the player.

---
+++ src/components/Controlbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Controlbar.module.css'
+
+const Controlbar = () => {
+  return (
+    <>
+        Controlbar
+    </>
+  )
+}
+
+export default Controlbar
