5c5510f85c9a8dc554ca4b097c6229f354c11d3a
victor <victoratnz@outlook.com>
2024-08-30 23:31:29 +1200

    Add Player component and styles
    The Player component and its associated styles have been added to the project. This component will be responsible for rendering the main content and control bar of the player. The styles have been defined in the new Player.module.css file.

---
+++ src/components/Player.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import styles from './Player.module.css'
+
+const Player = () => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.main}>
+                main
+            </div>
+            
+            <div className={styles.controlbar}>
+                <div>controlbar</div>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Player
