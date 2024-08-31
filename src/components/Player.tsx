e1a87f6095f312e161ff9639486c38810e5fdcfa
victor <victoratnz@outlook.com>
2024-08-31 17:58:43 +1200

    Update Player component

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,12 +1,30 @@
 import React from 'react'
 import styles from './Player.module.css'
 import Controlbar from './Controlbar'
+import music from '../assets/music.png'
+import { CirclePlus, Menu } from 'lucide-react'
 
 const Player = () => {
+
+    const onOpenFileList = () => {
+        
+    };
+
     return (
         <div className={styles.container}>
             <div className={styles.main}>
-                main
+                <div className={styles.lyric}>
+
+                    <img src={music} className='w-48' />
+
+                    <div className={styles.menu}>
+                        <Menu onClick={onOpenFileList} className={styles.openfilelist} />
+                        <CirclePlus className={styles.addfile} />
+                    </div>
+
+                </div>
+
+
             </div>
 
             <div className={styles.controlbar}>
